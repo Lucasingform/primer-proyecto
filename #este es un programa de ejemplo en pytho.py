a2107d068ee2715f642701a8ea3a50d1c3516f77
Lucasingform <lucas.elmeri771@gmail.com>
2025-04-08 20:10:57 -0400

    aprendiendo a usar python

---
+++ #este es un programa de ejemplo en pytho.py
@@ -0,0 +1,21 @@
+#este es un programa de ejemplo en python
+#para ejecutar una parte de este programa, deberas comentar las secciones que no probaras
+
+#variables y tipos de datos
+#aqui, se han creado variables que almacenan un numero entero.
+# un numero decimal, una cadena de texto y un valor booleano.
+variable_entera = 42 # 
+variable_decimal = 3.14 # Decimal
+variable_texto = "hola, mundo!" #cadena de texto
+variable_booleana = true # valor booleano 
+
+#operadores 
+
+#en este caso, resultado_suma almacena resultado de
+#sumar variable_entera y variable_decimal, y comparacion
+#almacena el resultado de comparar si variable_entera es
+#mayor que variable_decimal. 
+resultado_suma = variable_entera + variable_decimal
+comparacion = (variable_entera > variable_decimal)
+
+#entrada y salida
