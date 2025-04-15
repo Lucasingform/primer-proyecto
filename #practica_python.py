4bfab9fc68281cd3dd94a10adb5fac4cad7b0853
Lucasingform <lucas.elmeri771@gmail.com>
2025-04-14 20:18:49 -0400

    practica 2

---
+++ #practica_python.py
@@ -0,0 +1,28 @@
+#practica_python
+#solicitar al usuario que ingrese su nombre
+nombre = input("por favor, ingresa tu nombre: ")
+#imprimir un salduo personalidado
+print ("hola,", nombre, "¡bienvenido a python!.")
+
+#input es para solicitar entrada de caracteres, "ingrese su nombre"
+
+
+#numero entero
+edad=25
+
+#flotantes o numeros decimales 
+pi= 3.14159
+valor_UF = 36.088,46
+
+#cadenas de texto
+nombre = "juan"
+mensaje = "Hola, ¿como estas juan?, me gustaria conversar contigo...."
+
+#booleanos
+
+juan_mayor_edad = True 
+
+#ejemplos 
+nombre = "juan"
+edad= 25
+print ("la persona se llama: "+nombre)
